9d0fde734d06c039e0cc34e86eb940acef07b5f7
padi1234 <73397730+padi1234@users.noreply.github.com>
2021-02-14 12:22:52 +0100

    Initialize pokevision

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import 'react-s-alert/dist/s-alert-default.css'
+import 'react-s-alert/dist/s-alert-css-effects/stackslide.css'
+
+import React from 'react'
+import { render } from 'react-dom'
+
+import App from './app.js'
+import './styles/index.css'
+
+render(<App />, document.getElementById('app'))
